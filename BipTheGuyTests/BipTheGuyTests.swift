d91d0f9a207d8c4318eebe92527647bceab64a98
larry barrera <larrybarrera0066@gmail.com>
2026-04-08 11:13:02 -0700

    Initial Commit

---
+++ BipTheGuyTests/BipTheGuyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyTests.swift
+//  BipTheGuyTests
+//
+//  Created by BARRERA, LARRY on 4/8/26.
+//
+
+import Testing
+@testable import BipTheGuy
+
+struct BipTheGuyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
